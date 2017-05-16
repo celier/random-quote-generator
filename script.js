751728f18fa8bc3c8f918d021b9fc917103c4a5a
rceliegrimal <rceliegrimal@UNIV-NC.NC>
2017-05-16 11:35:52 +1100

    Corrected quote that wouldn't load on pageload and made a few change in the stylesheet

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-$(document).ready(function() {
+/***
+@name : random quote generator.
 
-    $( "#quote-btn" ).on("click", function() {
-       $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
+@objectives : Grabs a quote from API, print it to screen.
+
+***/
+
+
+function getQuote() {
+ 	 
+ 	 $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
       
-       $ ( "#quote" ).html("<i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>" + json[0].content + "<i class=\"fa fa-quote-right\" aria-hidden=\"true\"></i>");
+       $ ( "#quote" ).html(json[0].content);
        $ ( "#quote-author" ).html("A quote from " + json[0].title);
 
     });
+ }
+
+ getQuote();
+
+$(document).ready(function() {
+
+$( "#quote-btn" ).on("click", function() {
+
+      getQuote();
+
    });
 });
